fa8baebe5a86c1820eee777377b407f42d13a80c
Mihir <you@example.com>
2025-04-29 23:36:02 -0700

    save

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const config = require('./config');
+const config = require('./Config');
 const connectDB = require('./Config/db');
 const { errorHandler } = require('./middlewares/errorHandler');
 
